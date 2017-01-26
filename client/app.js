2ce6a5a7e651036afb72d96f58d0f51dd793dc64
lazyboy <upc_xbt@163.com>
2017-01-26 15:05:09 +0800

    resize app_main height

--- client/app.js
+++ client/app.js
@@ -5,7 +5,7 @@ var React = require('react')
 var App = React.createClass({
   render: function () {
     return <div className="app">
-      <div className="app_header">
+      <div id = "appId_header" className="app_header">
         <img src="logo.png" className="app_logo"/>
         <span className="app_title">Hexo Admin</span>
         <ul className="app_nav">
@@ -16,7 +16,7 @@ var App = React.createClass({
           <li><Link to="settings">Settings</Link></li>
         </ul>
       </div>
-      <div className="app_main">
+      <div id="appId_main" className="app_main">
         <this.props.activeRouteHandler/>
       </div>
     </div>;
